cb48c0a8794e28764a6e30285de79f786352c6ce
Azurock <azurock@debian>
2017-07-02 01:11:10 +0900

    feat(App): adds base for the app

--- app/src/components/header/index.js
+++ app/src/components/header/index.js
@@ -2,17 +2,68 @@ import { h, Component } from 'preact';
 import { Link } from 'preact-router/match';
 import style from './style';
 
-export default class Header extends Component {
-	render() {
-		return (
-			<header class={style.header}>
-				<h1>Preact App</h1>
-				<nav>
-					<Link activeClassName={style.active} href="/">Home</Link>
-					<Link activeClassName={style.active} href="/profile">Me</Link>
-					<Link activeClassName={style.active} href="/profile/john">John</Link>
-				</nav>
-			</header>
-		);
-	}
-}
+const Header = () => (<nav class={ `nav has-shadow ${style.isFixed}` }>
+  <div class="container">
+    <div class="nav-left">
+      <Link class="nav-item" href="/">
+        <img src="http://bulma.io/images/bulma-logo.png" alt="Bulma logo" />
+      </Link>
+			<Link class="nav-item is-tab is-hidden-mobile" activeClassName="is-active" href="/">Dashboard</Link>
+			<Link class="nav-item is-tab is-hidden-mobile" activeClassName="is-active" href="/whiteboard">Whiteboard</Link>
+      <Link class="nav-item is-tab is-hidden-mobile" activeClassName="is-active" href="/new">New</Link>
+		</div>
+		<div class="nav-center">
+			<div class="nav-item">
+	      <div class="field has-addons">
+	        <p class="control">
+	          <input class="input is-small" type="text" placeholder="Search" />
+	        </p>
+					<p class="control">
+				    <a class="button is-info is-small">
+				      <i class={ `fa fa-search ${style.isSmall}` }></i>
+				    </a>
+					</p>
+				</div>
+			</div>
+    </div>
+    <span class="nav-toggle">
+      <span></span>
+      <span></span>
+      <span></span>
+    </span>
+    <div class="nav-right nav-menu">
+			<Link class="nav-item is-tab is-hidden-tablet" activeClassName="is-active" href="/">Dashboard</Link>
+			<Link class="nav-item is-tab is-hidden-tablet" activeClassName="is-active" href="/whiteboard">Whiteboard</Link>
+      <Link class="nav-item is-tab is-hidden-tablet" activeClassName="is-active" href="/new">New</Link>
+      <Link class="nav-item is-tab" activeClassName="is-active" href="/profile">
+        <figure class="image is-32x32" style="margin-right: 8px;">
+          <img src="https://secure.gravatar.com/avatar/5c95e668902a93a7b6be802ba5ea85f5?d=404&s=160" />
+        </figure>
+        Profile
+      </Link>
+      <a class="nav-item is-tab">Log out</a>
+    </div>
+  </div>
+</nav>);
+
+export default  Header;
+
+
+
+
+
+
+// export default class Header extends Component {
+// 	render() {
+// 		return (
+// 			<header class={style.header}>
+// 				<h1>Nyart</h1>
+// 				<nav>
+// 					<Link activeClassName={style.active} href="/">Home</Link>
+// 					<Link activeClassName={style.active} href="/profile">Me</Link>
+// 					<Link activeClassName={style.active} href="/profile/john">John</Link>
+// 				</nav>
+// 			</header>
+// 		);
+// 	}
+// }
--- app/src/index.js
+++ app/src/index.js
@@ -1,4 +1,40 @@
 import './style';
-import App from './components/app';
+import 'font-awesome/css/font-awesome.min.css';
+// import App from './components/app';
+import 'bulma/css/bulma.css';
+
+import { h, Component } from 'preact';
+import { Router } from 'preact-router';
+
+import Header from './components/header';
+import Home from './pages/home';
+import Profile from './pages/profile';
+// import Profile from './routes/profile';
+// import Home from 'async!./home';
+// import Profile from 'async!./profile';
+
+class App extends Component {
+	/** Gets fired when the route changes.
+	 *	@param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
+	 *	@param {string} event.url	The newly routed URL
+	 */
+	handleRoute = e => {
+		this.currentUrl = e.url;
+	};
+
+	render() {
+		return (
+			<div id="app">
+				<Header />
+        <div class="main-container">
+  				<Router onChange={this.handleRoute}>
+  					<Home path="/" />
+            <Profile path="/profile" />
+  				</Router>
+        </div>
+			</div>
+		);
+	}
+}
 
 export default App;
